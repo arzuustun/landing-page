d3205d5a00873c016de67cae9c0017db573d9298
arzuustun <ustunarzu06@gmail.com>
2021-10-22 00:03:59 +0300

    UPDATE CODE

--- js/app.js
+++ js/app.js
@@ -1,39 +1,60 @@
 const sections = document.querySelectorAll("section");
-const menu =document.getElementById("navbar__list");
+
+let counter=0;
+function createSections(){
+counter++;
+let section =`<section id="section${counter}" data-nav="Section ${counter}" class="your-active-class">
+<div class="landing__container">
+  <h2>Section ${counter}</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
+
+  <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+</div>
+</section>`;
+document.querySelector('main').insertAdjacentHTML('beforeend',section);
+}
+for (let i=1; i<5; i++){
+    createSections();
+}
+
+createNavBarItem();
+
+const addSectionBtn=document.getElementById('btn');
+addSectionBtn.addEventListener('click',function(){
+    createSections();
+    document.getElementById("navbar__list").innerHTML="";
+    createNavBarItem();
+});
 
 function createNavBarItem(){
-    sections.forEach(function(item){
+    document.querySelectorAll("section").forEach(function(item){ 
         let sectionTitle= item.getAttribute('data-nav');
         let sectionId= item.getAttribute('id');
-        const li = document.createElement('li');
-        const a=document.createElement('a');
-        a.setAttribute('href','#' +sectionId);
-        a.className='menu__link';
-        const text =document.createTextNode(sectionTitle);
-        a.appendChild(text);
-        li.appendChild(a);
-        menu.appendChild(li);
+        let tag=`<li><a href=#${sectionId} data-nav="${sectionId}"class="menu__link"> ${sectionTitle}</a></li>`
+        document.getElementById("navbar__list").insertAdjacentHTML('beforeend',tag);
     });
  }
  
 function toggleSection(){
     sections.forEach(function(item){
         let sectionPositions= item.getBoundingClientRect();
-        if(sectionPositions.top >= 0 && sectionPositions.y <1){
+          if(sectionPositions.top >= -400 && sectionPositions.top <150){
+
            if (!item.classList.contains('your-active-class')){
                item.classList.add('your-active-class');
             }
         }
         else{
-            item.removeAttribute('class');
+           item.classList.remove('your-active-class');
         }
+
     });
 }
-createNavBarItem();
 document.addEventListener('scroll', toggleSection);
 
 
 /**
+ * 
  * 
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
