cb4b66c18c5517f36d1f2f4760345066c162a6e7
arzuustun <ustunarzu06@gmail.com>
2021-10-22 12:03:40 +0300

    updated

--- js/app.js
+++ js/app.js
@@ -1,17 +1,16 @@
 const sections = document.querySelectorAll("section");
-
 let counter=0;
 function createSections(){
-counter++;
-let section =`<section id="section${counter}" data-nav="Section ${counter}" class="your-active-class">
-<div class="landing__container">
-  <h2>Section ${counter}</h2>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
+    counter++;
+    let section =`<section id="section${counter}" data-nav="Section ${counter}" >
+    <div class="landing__container">
+    <h2>Section ${counter}</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
 
-  <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
-</div>
-</section>`;
-document.querySelector('main').insertAdjacentHTML('beforeend',section);
+    <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+    </div>
+    </section>`;
+    document.querySelector('main').insertAdjacentHTML('beforeend',section);
 }
 for (let i=1; i<5; i++){
     createSections();
@@ -35,26 +34,30 @@ function createNavBarItem(){
     });
  }
  
-function toggleSection(){
-    sections.forEach(function(item){
-        let sectionPositions= item.getBoundingClientRect();
-          if(sectionPositions.top >= -400 && sectionPositions.top <150){
+function isInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 
 
-           if (!item.classList.contains('your-active-class')){
-               item.classList.add('your-active-class');
-            }
-        }
-        else{
-           item.classList.remove('your-active-class');
-        }
+    );
+}
+
+document.addEventListener('scroll', function () {
+    document.querySelectorAll("section").forEach(function(box){
+        console.log(' isInViewport(box)', isInViewport(box));
+        if( isInViewport(box)) 
+        box.classList.add('your-active-class');
+        else
+        box.classList.remove('your-active-class');
 
     });
-}
-document.addEventListener('scroll', toggleSection);
-
+});
 
 /**
  * 
+ *  
+
  * 
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
